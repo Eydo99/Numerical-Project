a602fe10534401f4bf742713b0356c88aaebef4f
Saudox <midomido3363@gmail.com>
2025-11-22 22:14:45 +0200

    fixed round-sig

--- GaussElimination/back_substitution.py
+++ GaussElimination/back_substitution.py
@@ -10,11 +10,11 @@ class back_substitution:
     def solve(A, b, sig=6):
         n = len(b)
         x = [0]*n
-        x[n-1] = round(b[n-1] / A[n-1][n-1], sig)
+        x[n-1] = round_sig(b[n-1] / A[n-1][n-1], sig)
         for i in range(n-2, -1, -1):
             tot = 0
             for j in range(i+1,n):
-                tot +=  round(A[i][j] * x[j], sig)
-            x[i] =  round((b[i]-tot)/A[i][i], sig)
+                tot +=  round_sig(A[i][j] * x[j], sig)
+            x[i] =  round_sig((b[i]-tot)/A[i][i], sig)
 
         return x
