95eae71882c3aebcb1ab20378954db90eab615fe
nelatawy <nour.atawy2015@gmail.com>
2025-12-10 23:07:29 +0200

    added the plotter, restructured the file hierarchy, seperated the routes into lse and rootfinder as blueprints

---
+++ RootFinding/utils/auxilary.py
@@ -0,0 +1,12 @@
+from sympy import *
+import math
+
+def get_lambda_func ( func_str : str) :
+    x = Symbol('x')
+    func_simpified = sympify(func_str)
+    return lambdify(x, func_simpified, "numpy")
+
+def round_sig(x, sig=6):
+    if x == 0:
+        return 0.0
+    return round(x, sig - int(math.floor(math.log10(abs(x)))) - 1)
